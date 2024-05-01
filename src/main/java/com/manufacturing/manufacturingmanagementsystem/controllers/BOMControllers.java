be361c10d245e9d40e6aab483925930a7c1d409f
Cevan <110558291+Cevan23@users.noreply.github.com>
2024-05-02 00:40:30 +0700

    fix push

--- src/main/java/com/manufacturing/manufacturingmanagementsystem/controllers/BOMControllers.java
+++ src/main/java/com/manufacturing/manufacturingmanagementsystem/controllers/BOMControllers.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api/roles")
+@RequestMapping("/api/BOMs")
 public class BOMControllers {
 
     @Autowired
